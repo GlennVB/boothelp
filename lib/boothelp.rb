706121fc8a44eb755a8ddb7b6681e439cbf9c177
Faolan <faolan0@gmail.com>
2014-10-27 12:38:17 +0100

    Update six

--- lib/boothelp.rb
+++ lib/boothelp.rb
@@ -1,8 +1,20 @@
 require "boothelp/version"
 
 module Boothelp
-  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers
+  include ActionView::Context
+  
+  extend ActionView::Helpers
+  extend ActionView::Context
   def self.icon(*args)
+    options = args.extract_options!
+    icon = args.first.to_s
+ 
+    return content_tag(:i, :class => "fa #{icon} fa-fw") do
+    end
+  end
+  
+  def icon(*args)
     options = args.extract_options!
     icon = args.first.to_s
  
